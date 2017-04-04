10ef971fded1596bb15e64f71d8005533250ffd4
Mohammad-Ali AʼRÄBI <aerabi@gmx.de>
2017-04-04 03:17:17 +0200

    Install Travis-CI

--- smac.py
+++ smac.py
@@ -1,4 +1,4 @@
-from __future__ import division
+from __future__ import division, print_function
 
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
@@ -25,10 +25,10 @@ def tree_classification_precision(criterion='gini', splitter='best', max_feature
 
 
 if __name__ == '__main__':
-    print 1 - tree_classification_precision()
-    print 1 - tree_classification_precision(criterion='entropy')
-    print 1 - tree_classification_precision(splitter='random')
-    print 1 - tree_classification_precision(criterion='entropy', splitter='random')
+    print(1 - tree_classification_precision())
+    print(1 - tree_classification_precision(criterion='entropy'))
+    print(1 - tree_classification_precision(splitter='random'))
+    print(1 - tree_classification_precision(criterion='entropy', splitter='random'))
 
     parameters = dict(
         criterion=('categorical', ['gini', 'entropy'], 'gini'),
@@ -40,4 +40,5 @@ if __name__ == '__main__':
 
     opt = pysmac.SMAC_optimizer()
     value, parameters = opt.minimize(tree_classification_precision, 1000, parameters)
-    print (1 - value) * 100.0, parameters
+    print((1 - value) * 100.0)
+    print(parameters)

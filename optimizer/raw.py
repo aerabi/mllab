958ec877cf9c89170360d0844cbe55975c5d93a0
Mohammad-Ali AʼRÄBI <aerabi@gmx.de>
2018-03-20 22:49:58 +0100

    Implement RAndom sampler RAW

---
+++ optimizer/raw.py
@@ -0,0 +1,33 @@
+class Raw:
+    def __init__(self):
+        self.keys = []
+        self.distributions = []
+
+    def remove(self, key):
+        if key in self.keys:
+            index = self.keys.index(key)
+            self.keys.pop(index)
+            self.distributions.pop(index)
+
+    def add(self, key, distribution):
+        self.remove(key)
+        self.keys.append(key)
+        self.distributions.append(distribution)
+
+    def add_multi(self, keys, multi_distribution):
+        for key in keys:
+            self.remove(key)
+        self.keys.append(keys)
+        self.distributions.append(multi_distribution)
+
+    def sample(self):
+        dick = {}
+        for i in range(len(self.keys)):
+            if isinstance(self.keys[i], list):
+                keys_keys = self.keys[i]
+                vals_vals = self.distributions[i].resample(size=1)
+                for j in range(len(keys_keys)):
+                    dick[keys_keys[j]] = vals_vals[j][0]
+            else:
+                dick[self.keys[i]] = self.distributions[i].rvs()
+        return dick

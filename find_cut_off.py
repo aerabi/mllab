0dafb3a9fe6a90575c20970042e0e3bebe4f6c4e
Mohammad-Ali AʼRÄBI <aerabi@gmx.de>
2018-05-08 09:17:27 +0200

    Add load and plot

--- find_cut_off.py
+++ find_cut_off.py
@@ -1,4 +1,7 @@
+import argparse
 import json
+import matplotlib.pyplot as plt
+import numpy as np
 import subprocess
 
 
@@ -37,5 +40,41 @@ def main(iter=5, input_file='cluster/rawAllx1000.json', cutoffs=range(5, 100, 5)
     return result
 
 
+def load(cutoffs=range(5, 100, 5), aggregate_function=np.mean, score_function='accuracy'):
+    all_scores = {}
+    for i in cutoffs:
+        scores = []
+        with open('cuts/cut%02d.json' % i, 'r') as json_file:
+            data = json.load(json_file)
+            for key, datum in data.items():
+                score = aggregate_function([x['scores']['%s_score' % score_function] for x in datum])
+                scores.append(score)
+        all_scores[i] = np.mean(scores)
+    return all_scores
+
+
 if __name__ == '__main__':
-    json.dump(main(), open('cutoffs.json', 'w'))
+    cmd_parser = argparse.ArgumentParser('workstation')
+    subparsers = cmd_parser.add_subparsers(dest='option')
+    calc_parser = subparsers.add_parser('calc')
+    load_parser = subparsers.add_parser('load')
+    load_parser.add_argument('-m', '--metric', default='accuracy', choices=['accuracy', 'f1', 'recall', 'precision'])
+    load_parser.add_argument('-f', '--function', default='np.mean', help='aggregator for each dataset, default=np.mean')
+    args = cmd_parser.parse_args()
+    if args.option == 'calc':
+        json.dump(main(), open('cutoffs.json', 'w'))
+    else:
+        aggregate_function = eval(args.function)
+        score_function = args.metric
+        all_scores = load(score_function=score_function, aggregate_function=aggregate_function)
+
+        # plot
+        ks, vs = [], []
+        for k, v in all_scores.items():
+            ks.append(k)
+            vs.append(v)
+            print(k, v)
+        plt.plot(ks, vs)
+        plt.xlabel('Cutoff')
+        plt.ylabel(score_function.capitalize())
+        plt.savefig('cutoff-%s-%s.pdf' % (score_function, aggregate_function.__name__))

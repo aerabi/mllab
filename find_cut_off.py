3a8cce9c319fc16e323788e818003b35dccb6e48
Mohammad-Ali AʼRÄBI <aerabi@gmx.de>
2018-05-08 23:23:02 +0200

    Add arguments to calc and support for random seed

--- find_cut_off.py
+++ find_cut_off.py
@@ -1,16 +1,18 @@
 import argparse
 import json
+import logging
 import matplotlib.pyplot as plt
 import numpy as np
 import subprocess
 
 
-def main(iter=5, input_file='cluster/rawAllx1000.json', cutoffs=range(5, 100, 5)):
+def main(iter=5, input_file='cluster/rawAllx1000.json', cutoffs=range(5, 100, 5), seeds=[1]):
     """
     create KDEs with different cutoffs, test them and return the result
     :param iter: number of iterations on each dataset, in testing phase
     :param input_file: the name of the json input file
     :param cutoffs: iterable of cutoffs, in integer
+    :param seeds: list of randoms seeds
     :return: f1 score for each of the cutoffs
     """
     features = [
@@ -25,18 +27,25 @@ def main(iter=5, input_file='cluster/rawAllx1000.json', cutoffs=range(5, 100, 5)
         subprocess.run(['python', 'workstation.py', 'load', input_file,
                         '-H'] + features + [
                         '-s', '%.2f' % i, '-S', kde_name])
-        output_file = open('cuts/cut%02d.o' % i, 'w')
-        subprocess.run(['python', 'workstation.py', 'calc', '--raw', '-i', str(iter),
-                        '-C', '|'.join(features),
-                        # '-t'] + list(map(str, tasks)) + [
-                        kde_name, '"lambda *xs : xs[0] > 0.5, x[1], int(round(x[2])), int(round(x[3]))"',
-                        '--save', 'cuts/cut%02d.json' % i], stdout=output_file)
-        output_file.close()
-        with open('cuts/cut%02d.o' % i, 'r') as f:
-            o = f.read()
-        score = o.split(' ')[-1].strip()
-        result.append([i, float(score)])
-        print(i, score)
+        for seed in seeds:
+            output_file_name = 'cuts/cut%02d-%d.o' % (i, seed)
+            with open(output_file_name, 'w') as output_file:
+                arguments = [
+                    'python', 'workstation.py', 'calc', '--raw', '-i', str(iter),
+                    '-C', '|'.join(features),
+                    kde_name, '"lambda *xs : xs[0] > 0.5, x[1], int(round(x[2])), int(round(x[3]))"',
+                    '--seed', str(3 ** seed),
+                    '--save', 'cuts/cut%02d-%d.json' % (i, seed)
+                ]
+                logging.debug(' '.join(arguments))
+                r = subprocess.run(arguments, stdout=output_file, stderr=subprocess.PIPE)
+            if len(r.stderr) > 2:
+                logging.error(r.stderr)
+            with open(output_file_name, 'r') as f:
+                o = f.read()
+            score = o.split(' ')[-1].strip()
+            result.append([i, float(score)])
+            print(i, score)
     return result
 
 
@@ -54,27 +63,37 @@ def load(cutoffs=range(5, 100, 5), aggregate_function=np.mean, score_function='a
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
     cmd_parser = argparse.ArgumentParser('workstation')
     subparsers = cmd_parser.add_subparsers(dest='option')
     calc_parser = subparsers.add_parser('calc')
+    calc_parser.add_argument('-i', '--iter', default=5, type=int, help='number of iterations on each of the datasets')
+    calc_parser.add_argument('-s', '--seed', default=[1], type=int, nargs='+', help='random seeds')
+    calc_parser.add_argument('-c', '--cutoff', default=[5], type=int, nargs='+', help='cutoffs')
+    calc_parser.add_argument('-S', '--save', default='cutoff.json', type=str, help='output json file')
     load_parser = subparsers.add_parser('load')
-    load_parser.add_argument('-m', '--metric', default='accuracy', choices=['accuracy', 'f1', 'recall', 'precision'])
+    load_parser.add_argument('-m', '--metric', default=['accuracy'], nargs='+',
+                             choices=['accuracy', 'f1', 'recall', 'precision'])
     load_parser.add_argument('-f', '--function', default='np.mean', help='aggregator for each dataset, default=np.mean')
     args = cmd_parser.parse_args()
     if args.option == 'calc':
-        json.dump(main(), open('cutoffs.json', 'w'))
+        json.dump(main(iter=args.iter, cutoffs=args.cutoff, seeds=args.seed), open(args.save, 'w'))
     else:
+        colors = {'accuracy': 'blue', 'f1': 'red', 'precision': 'purple', 'recall': 'green'}
         aggregate_function = eval(args.function)
-        score_function = args.metric
-        all_scores = load(score_function=score_function, aggregate_function=aggregate_function)
+        legend = []
+        for score_function in args.metric:
+            all_scores = load(score_function=score_function, aggregate_function=aggregate_function)
 
-        # plot
-        ks, vs = [], []
-        for k, v in all_scores.items():
-            ks.append(k)
-            vs.append(v)
-            print(k, v)
-        plt.plot(ks, vs)
+            # plot
+            ks, vs = [], []
+            for k, v in all_scores.items():
+                ks.append(k)
+                vs.append(v)
+                print(k, v)
+            line, = plt.plot(ks, vs, label=score_function.capitalize(), color=colors[score_function])
+            legend.append(line)
+        plt.legend(handles=legend, loc=1)
         plt.xlabel('Cutoff')
-        plt.ylabel(score_function.capitalize())
-        plt.savefig('cutoff-%s-%s.pdf' % (score_function, aggregate_function.__name__))
+        plt.ylabel('Scores')
+        plt.savefig('cutoff-%s-%s.pdf' % ('-'.join(args.metric), aggregate_function.__name__))

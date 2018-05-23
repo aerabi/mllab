a7c857cd101b6f03bf783bfc7384f8c222c51957
Mohammad-Ali AʼRÄBI <aerabi@gmx.de>
2018-05-24 00:34:26 +0200

    Update the plotting script

--- find_cut_off_print.py
+++ find_cut_off_print.py
@@ -4,21 +4,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-if __name__ == '__main__':
+def load_data(name_template='cluster_cuts/cutoff%02d.json', interval=range(5, 100, 5), func=np.median):
     j = []
-    for i in range(5, 100, 5):
-        with open('cuts/cutoff%02d.json' % i, 'r') as f:
+    for i in interval:
+        with open(name_template % i, 'r') as f:
             j += json.load(f)
     j = [x for x in j if 0.0 < x[1] < 1.0]
     m = defaultdict(list)
     for x in j:
         m[x[0]].append(x[1])
 
-    X = [m[i] for i in range(5, 100, 5)]
-    means = [np.median(x) for x in X]
+    X = [m[i] for i in interval]
+    means = [func(x) for x in X]
+
+    return X, means
+
+
+if __name__ == '__main__':
+    reihe = range(5, 100, 5)
+    func = np.median
+    X, means = load_data(interval=reihe, func=func)
+    Y, moans = load_data(name_template='cluster_cuts/cutoff1d%02d.json', interval=reihe, func=func)
 
     plt.boxplot(X)
-    plt.xticks(list(range(1, 20)), map(str, list(range(5, 100, 5))))
-    plt.plot(list(range(1, 20)), means)
+    plt.xticks(list(range(1, len(reihe) + 1)), map(str, list(reihe)))
+    plt.plot(list(range(1, len(reihe) + 1)), means)
+    plt.plot(list(range(1, len(reihe) + 1)), moans, color='red')
     plt.grid()
     plt.show()
